0d29fc1f2e64491d8eff0b577d0307b2656ec4bf
amitnarayansah <amitnarayansah@gmail.com>
2019-08-23 13:03:28 +0530

    bug fixed

--- src/WeMoSwitch.h
+++ src/WeMoSwitch.h
@@ -9,33 +9,30 @@
 #include "CallbackFunction.h"
 //#include "WebServer.h"
 #include "WeMo.h"
+#include"Util.h"
 //#include "SmartSwitch.h"
 
 class WeMoSwitch
 {
 private:
-  //boolean wifiConnected = false;
   int counter = 10;
-  Witty wittyB;
-
-public:
-
   Switch *relaySwitch[WeMo::NoOfRelay];
   UpnpBroadcastResponder upnpBroadcastResponder;
+ //Callback function for On/Off
+static bool SwitchOn(int );
+static bool SwitchOff(int);
 
+
+ public:
   // Main Functions
   bool initWemoSwitch();
   void wemoSwitchLoop();
 
   //Constructor and Destructor
-  WeMoSwitch(Witty b);
+  WeMoSwitch();
   ~WeMoSwitch();
 
-  //Callback function for On/Off
-static bool SwitchOn(int );
-static bool SwitchOff(int);
-
-
+ 
 
 }; //End of Class
 
